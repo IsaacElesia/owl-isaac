938994015e06da95664ef21a3749c903eb16cf3c
Isaac Elesia <isaacelesia@gmail.com>
2020-04-18 08:24:52 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './components/List.js';
+import Stage from './components/Stage';
+import Chat from './components/Chat';
+import './App.css'
+import './Stage.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  static defaultProps = {
+    store: {
+      participants: [ ],
+      chatEvents : []
+    },
+  };
+
+
+  render() {
+    const { store } = this.props
+    return (
+      <main className='App'>
+        <div className="App-list">
+          {store.participants.filter(participant=> (participant.inSession)).map(participant =>
+            <List
+              key={participant.id}
+              name={participant.name}
+              avatar={participant.avatar}
+              inSession={participant.inSession}
+              onStage={participant.onStage}
+            />
+          )}
+          {store.participants.filter(participant=> (!participant.inSession)).map(participant =>
+            <List
+              key={participant.id}
+              name={participant.name}
+              avatar={participant.avatar}
+              inSession={participant.inSession}
+              onStage={participant.onStage}
+            />
+          )}
+        </div>
+        <div className="App-chat">
+          {store.chatEvents.map(chatEvents =>
+            <Chat
+              name={store.participants.find(val=> val.id===chatEvents.participantId).name}
+              avatar={store.participants.find(val=> val.id===chatEvents.participantId).avatar}
+              type={chatEvents.type}
+              message={chatEvents.message}
+              time={chatEvents.time}
+              timestamp={chatEvents.timestamp}
+            />
+          )}
+        </div>
+        <div className='app-stage'>
+            {store.participants.filter(participant => participant.onStage).map(participant => 
+              <Stage
+                key={participant.id}
+                avatar={participant.avatar}
+                name={participant.name}
+              />
+            )}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
